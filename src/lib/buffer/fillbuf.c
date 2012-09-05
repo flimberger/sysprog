0c721b21f73e13039e7ba88a4bc6d5c7ca4047a2
Florian Limberger <flo@snakeoilproductions.net>
2012-09-05 14:33:29 +0200

    Implement fillbuf

--- src/lib/buffer/fillbuf.c
+++ src/lib/buffer/fillbuf.c
@@ -5,7 +5,16 @@
 int
 fillbuf(Buffer *buf)
 {
-	buf->flags |= Clean;
+	ssize_t i;
+
+	if ((buf->flags & Clean) == 0)
+		return EOF;	/* TODO: errno? */
+
+	if ((i = read(buf->fd, buf->bsb, buf->size)) <= 0)
+		return EOF;	/* errno set by read(), if an error occured */
+
+	buf->esb = buf->bsb + i - 1;
+	buf->flags &= ~Clean;
 	return 0;
 }
 
