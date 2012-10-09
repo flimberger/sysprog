8fd0219a7bd59fcf5d9f42138ac5052d2d280d32
Florian Limberger <flo@snakeoilproductions.net>
2012-10-09 17:45:17 +0200

    Fix buffer output

--- src/lib/buffer/bflush.c
+++ src/lib/buffer/bflush.c
@@ -8,8 +8,10 @@ bbflush(Buffer *buf)
 	if (bflush(buf) == EOF)
 		return EOF;
 
-	if (buf->nc > buf->bpb)
+	if (buf->nc > buf->bpb) {
+		buf->epb = buf->nc - 1;
 		buf->state = Dirty;
+	}
 
 	swtchbuf(buf);
 
@@ -20,23 +22,18 @@ int
 bflush(Buffer *buf)
 {
 	ptrdiff_t d;
-	ssize_t i;
 
 	if (buf->state == Clean)
 		return 0;
 
 	if (buf->flags & Writebuf) {
-		d = buf->esb - buf->bsb;
-		for (i = 0; i < Bufsize; i += Blocksize)
-			if (i > d) {
-				if (write(buf->fd, buf->bsb, i) != i) {
-					buf->flags &= ~Active;
-					buf->nc = buf->bsb;
-					return EOF; /* errno set by write() */
-				}
-				buf->state = Clean;
-				return 0;
-			}
+		d = buf->esb - buf->bsb + 1;
+		if (write(buf->fd, buf->bsb, d) != d) {
+			buf->flags &= ~Active;
+			buf->nc = buf->bsb;
+			return EOF; /* errno set by write() */
+		}
+		buf->state = Clean;
 	}
 
 	return 0;
