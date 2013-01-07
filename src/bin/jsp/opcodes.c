b8e022ecf19066912f566b065a502188b5a9774b
Florian Limberger <flo@snakeoilproductions.net>
2013-01-07 19:45:27 +0100

    Add opcodes for read and print

--- src/bin/jsp/opcodes.c
+++ src/bin/jsp/opcodes.c
@@ -1,17 +1,55 @@
 #include "opcodes.h"
 
 char *opcodenames[] = {
-	"ldc",
+/* 0x00 */	"nop",
+/* 0x01 */	"aconst_null"
+/* 0x02 */	"iconst_m1",
+/* 0x03 */	"iconst_0",
+/* 0x04 */	"iconst_1",
+/* 0x05 */	"iconst_2",
+/* 0x06 */	"iconst_3",
+/* 0x07 */	"iconst_4",
+/* 0x08 */	"iconst_5",
 
-	"aload_0",
-	"aload_1",
-	"aload_2",
-	"aload_3",
+/* 0x12 */	"ldc",
 
-	"return",
-	"getstatic",
+/* 0x15 */	"iload",
 
-	"invokevirtual",
-	"invokespecial",
-	"invokestatic"
+/* 0x1a */	"iload_0",
+/* 0x1b */	"iload_1",
+/* 0x1c */	"iload_2",
+/* 0x1d */	"iload_3",
+
+/* 0x2a */	"aload_0",
+/* 0x2b */	"aload_1",
+/* 0x2c */	"aload_2",
+/* 0x2c */	"aload_3",
+
+/* 0x36 */	"istore",
+
+/* 0x3a */	"astore",
+/* 0x3b */	"istore_0",
+/* 0x3c */	"istore_1",
+/* 0x3d */	"istore_2",
+/* 0x3e */	"istore_3",
+
+/* 0x4b */	"astore_0",
+/* 0x4c */	"astore_1",
+/* 0x4d */	"astore_2",
+/* 0x4e */	"astore_3",
+
+/* 0xa7 */	"goto",
+
+/* 0x59 */	"dup",
+
+/* 0xac */	"ireturn",
+
+/* 0xb1 */	"return",
+/* 0xb2 */	"getstatic",
+
+/* 0xb6 */	"invokevirtual",
+/* 0xb7 */	"invokespecial",
+/* 0xb8 */	"invokestatic",
+
+/* 0xbb */	"new"
 };
--- src/bin/jsp/opcodes.h
+++ src/bin/jsp/opcodes.h
@@ -4,12 +4,48 @@
 /* http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-6.html */
 
 typedef enum {
+	NOP       = 0x00,
+	ACONST_NULL = 0x01,
+	ICONST_M1 = 0x02,
+	ICONST_0  = 0x03,
+	ICONST_1  = 0x04,
+	ICONST_2  = 0x05,
+	ICONST_3  = 0x06,
+	ICONST_4  = 0x07,
+	ICONST_5  = 0x08,
+
 	LDC = 0x12,
+
+	ILOAD = 0x15,
+
+	ILOAD_0 = 0x1a,
+	ILOAD_1 = 0x1b,
+	ILOAD_2 = 0x1c,
+	ILOAD_3 = 0x1d,
 
 	ALOAD_0 = 0x2a,
 	ALOAD_1 = 0x2b,
 	ALOAD_2 = 0x2c,
 	ALOAD_3 = 0x2d,
+
+	ISTORE = 0x36,
+
+	ASTORE   = 0x3a,
+	ISTORE_0 = 0x3b,
+	ISTORE_1 = 0x3c,
+	ISTORE_2 = 0x3d,
+	ISTORE_3 = 0x3e,
+
+	ASTORE_0 = 0x4b,
+	ASTORE_1 = 0x4c,
+	ASTORE_2 = 0x4d,
+	ASTORE_3 = 0x4e,
+
+	DUP = 0x59,
+
+	GOTO = 0xa7,
+
+	IRETURN = 0xac,
 
 	RETURN = 0xb1,
 	GETSTATIC = 0xb2,
@@ -17,6 +53,8 @@ typedef enum {
 	INVOKEVIRTUAL = 0xb6,
 	INVOKESPECIAL = 0xb7,
 	INVOKESTATIC  = 0xb8,
+
+	NEW = 0xbb
 } Opcode;
 
 extern char *opcodenames[];
